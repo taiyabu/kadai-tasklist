df57a1e617573cb163d92fcfe4de9c5e6699d5e6
taichi yabuno <1510180091e@kindai.ac.jp>
2017-12-25 12:38:46 +0000

    last

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,51 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = 'タスクの登録に成功しました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:success] = 'タスクの編集に成功しました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクの編集に失敗しました。'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_url
+  end
+  
+  private
+  
+  def task_params
+    params.requre(:task).permit(:content)
+  end
+end
